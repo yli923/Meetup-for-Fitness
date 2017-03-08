aff49864265bcad7c3eb917057c4cc14b1a11950
Yichen Li <yichenxk@gmail.com>
2017-03-08 01:03:33 -0500

    signup method finished

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ mysql.init_app(app)
 @app.route('/auth/login',methods=['POST'])
 def auth_login():
 	if not request.json or not 'username' in request.json or not 'password' in request.json:
-		abort(400)
+		abort(400, '{"message":"false"}')
 	username = request.json['username']
 	password = request.json['password']
 	db = mysql.connect()
@@ -33,8 +33,38 @@ def auth_login():
 	else: 
 		db.rollback()
 		db.close()
-		abort(400, '{"message":"false"}') 
+		abort(400, '{"message":"login failed"}') 
+
+@app.route('/user/info', methods=['POST'])
+def add_user_info():
+	if not request.json or not 'username' in request.json or not 'password' in request.json or not 'gender' in request.json or not 'email' in request.json or not 'avatarURL' in request.json or not 'description' in request.json:
+		abort(400, '{"message":"Input parameter incorrect or missing"}')
+	username = request.json['username']
+	password = request.json['password']
+	gender = request.json['gender']
+	email = request.json['email']
+	avatarURL = request.json['avatarURL']
+	description = request.json['description']
+	db = mysql.connect()
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM User WHERE username = '%s'"%username)
+	if cursor.rowcount == 0:
+		try:
+			cursor.execute("INSERT INTO User(username,password,gender,email,avatarURL,description) values (%s,%s,%s,%s,%s,%s)",[username,password,gender,email,avatarURL,description])
+			userId = cursor.lastrowid
+			db.commit()
+			db.close()
+			return 'Insert User Info Success'
+		except:
+			db.rollback()
+	   		db.close()
+	   		abort(400, '{"message":"insert unsuccessful"}')
+	else:
+		db.rollback()
+		db.close()
+		abort(400, '{"message":"user exists"}')
+
 
 if __name__ == '__main__':
-	# app.run(host='0.0.0.0',port='80')
-	app.run()
+	app.run(host='0.0.0.0',port='80')
+	#app.run()
